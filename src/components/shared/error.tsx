c3c9ec5e92fbf20a4587c70c1100871aa01b4d0d
tad dy <a@a.com>
2023-05-11 21:36:18 -0400

    convert some <a/> tags to <Link/> tags

--- src/components/shared/error.tsx
+++ src/components/shared/error.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from "react";
+import Link from "next/link";
 
 export function Error() {
   return (
@@ -12,15 +13,15 @@ export function Error() {
           Sorry! We are looking into this now.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
+          <Link
             href="/"
             className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
           >
             Go back home
-          </a>
-          <a href="/" className="text-sm font-semibold text-gray-900">
+          </Link>
+          <Link href="/" className="text-sm font-semibold text-gray-900">
             Contact support <span aria-hidden="true">&rarr;</span>
-          </a>
+          </Link>
         </div>
       </div>
     </main>
