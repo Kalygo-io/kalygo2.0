04319c783e5f8dd9c89605f4d4a6e76b19e381d8
Taddy <a@a.com>
2023-05-15 17:16:25 -0400

    added a general feedback form

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 const navigation = [
   { name: "Use Cases", href: "/use-cases" },
   { name: "Contracts", href: "/use-cases" },
+  { name: "Feedback", href: "/feedback" },
 ];
 
 export function Navbar() {
