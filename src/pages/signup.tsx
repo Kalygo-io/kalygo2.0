f1b9ee348841bb1f04a8015e15040604a9cf7d91
Taddy <a@a.com>
2023-05-15 13:43:53 -0400

    saving addition of email and password

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -4,7 +4,8 @@ import { useForm } from "react-hook-form";
 import Layout1 from "@/layout/layout1";
 import Head from "next/head";
 import Link from "next/link";
-import { useEffect } from "react";
+// import { useEffect } from "react";
+import axios from "axios";
 import Image from "next/image";
 
 import { toast } from "react-toastify";
@@ -20,23 +21,39 @@ export default function Signup() {
   } = useForm({
     defaultValues: {
       email: "",
+      password: "",
     },
   });
 
   const onSubmit = async (data: any) => {
-    const { email } = data;
+    try {
+      const { email, password } = data;
+      console.log("data", data);
 
-    console.log("email", email);
+      var config = {
+        method: "post",
+        url: `${process.env.NEXT_PUBLIC_API_HOSTNAME}/api/v1/auth/sign-up`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          email,
+          password,
+        },
+      };
 
-    toast("Wow so easy!");
+      // debugger;
 
-    // throw new Error("___ _!_!_ ___");
+      let resp = await axios(config);
 
-    // try {
-    //   console.log("___ ___ ___");
-    // } catch (e) {
-    //   console.error(e);
-    // }
+      // debugger;
+
+      console.log(resp);
+
+      toast("Wow so easy!");
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -62,29 +79,72 @@ export default function Signup() {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
             <div>
-              <label htmlFor="email" className="sr-only">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
                 Email address
               </label>
-              <input
-                {...register("email", {
-                  required: true,
-                  pattern: new RegExp(
-                    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-                  ),
-                })}
-                id="email"
-                name="email"
-                placeholder="Enter your email"
-                autoComplete="email"
-                className={`block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 ${
-                  errors["email"] && "ring-red-700 focus:ring-red-500"
-                }`}
-              />
+              <div className="mt-2">
+                <input
+                  {...register("email", {
+                    required: true,
+                    pattern: new RegExp(
+                      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+                    ),
+                  })}
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  placeholder="Enter email"
+                  className={`block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 ${
+                    errors["email"] && "ring-red-700 focus:ring-red-500"
+                  }`}
+                />
+              </div>
             </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  {...register("password", {
+                    required: true,
+                    minLength: 7,
+                  })}
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  placeholder="Minimum 7 characters"
+                  // className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className={`block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 ${
+                    errors["password"] && "ring-red-700 focus:ring-red-500"
+                  }`}
+                />
+              </div>
+            </div>
+
             <div>
               <button
                 // type="submit"
-                onClick={() => {}}
+                // onClick={() => onSubmit(getValues())}
                 className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
               >
                 Sign up
