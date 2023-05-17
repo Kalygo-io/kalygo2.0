a0a21fd49f96fa7bee36fde8c0dd735d7754f326
Taddy <a@a.com>
2023-05-17 04:17:39 -0400

    disable the @next/next/no-html-link-for-pages rule for now

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -27,6 +27,7 @@ export function Navbar() {
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
+          {/* // eslint-disable-line @next/next/no-html-link-for-pages */}
           <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">{t("common:company-name")}</span>
             <Image
