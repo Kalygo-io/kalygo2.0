869d2e37d32f26bf45f295147bb90c0fe68cc494
Taddy <a@a.com>
2023-05-19 10:32:43 -0400

    adding a marketing section to the landing page

---
+++ src/components/indexComponents/stats.tsx
@@ -0,0 +1,62 @@
+export function Stats() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            We approach justice as something that should be accesible to
+            everyone.
+          </h2>
+          <p className="mt-6 text-base leading-7 text-gray-600">
+            The traditional legal system is obtuse, convoluted, and expensive
+            which is where Kalygo comes into the picture.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
+          <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-50 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start">
+            <p className="flex-none text-3xl font-bold tracking-tight text-gray-900">
+              387
+            </p>
+            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+              <p className="text-lg font-semibold tracking-tight text-gray-900">
+                Visitors to our site
+              </p>
+              <p className="mt-2 text-base leading-7 text-gray-600">
+                In cases where you have issues finding appropriate legal
+                partners for your deal, Kalygo members can securely move your
+                case forward.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-900 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44">
+            <p className="flex-none text-3xl font-bold tracking-tight text-white">
+              100%
+            </p>
+            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+              <p className="text-lg font-semibold tracking-tight text-white">
+                Kalygo prides itself on customers and users being satisfied in a
+                quick and meaningful way.
+              </p>
+              <p className="mt-2 text-base leading-7 text-gray-400">
+                Satisfaction is our priority
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-indigo-600 p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
+            <p className="flex-none text-3xl font-bold tracking-tight text-white">
+              $1,000,000,000+
+            </p>
+            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+              <p className="text-lg font-semibold tracking-tight text-white">
+                Anticipated savings for members of the Kalygo platform
+              </p>
+              <p className="mt-2 text-base leading-7 text-indigo-200">
+                Transparency is key to how Kalygo works
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
