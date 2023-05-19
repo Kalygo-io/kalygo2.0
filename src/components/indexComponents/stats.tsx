ab5a07738840af0fadd237c27df78e4c179dd0a6
Taddy <a@a.com>
2023-05-19 10:45:52 -0400

    color consistency

--- src/components/indexComponents/stats.tsx
+++ src/components/indexComponents/stats.tsx
@@ -42,7 +42,7 @@ export function Stats() {
               </p>
             </div>
           </div>
-          <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-indigo-600 p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
+          <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-blue-600 p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
             <p className="flex-none text-3xl font-bold tracking-tight text-white">
               $1,000,000,000+
             </p>
@@ -50,7 +50,7 @@ export function Stats() {
               <p className="text-lg font-semibold tracking-tight text-white">
                 Anticipated savings for members of the Kalygo platform
               </p>
-              <p className="mt-2 text-base leading-7 text-indigo-200">
+              <p className="mt-2 text-base leading-7 text-blue-200">
                 Transparency is key to how Kalygo works
               </p>
             </div>
