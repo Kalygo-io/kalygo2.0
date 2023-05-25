db23f19acd93be93cec4b41168cda5365aaf5c84
Taddy <a@a.com>
2023-05-25 18:20:05 -0400

    save suggestions from Luciano

--- src/pages/[locale]/dashboard/index.tsx
+++ src/pages/[locale]/dashboard/index.tsx
@@ -37,15 +37,15 @@ export default function Dashboard() {
   const { state, dispatch } = useAppContext();
   const { t } = useTranslation();
 
-  const contracts = [
-    {
-      name: "NDA",
-      title: "NDA with Lindsay",
-    },
-    {
-      name: "Lease",
-      title: "Lease on 302 Meridian Ave.",
-    },
+  const contracts: any[] = [
+    // {
+    //   name: "NDA",
+    //   title: "NDA with Lindsay",
+    // },
+    // {
+    //   name: "Lease",
+    //   title: "Lease on 302 Meridian Ave.",
+    // },
   ];
 
   return (
@@ -73,50 +73,55 @@ export default function Dashboard() {
           <div className="mt-8 flow-root">
             <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                <table className="min-w-full divide-y divide-gray-300">
-                  <thead>
-                    <tr>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
-                      >
-                        Name
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                      >
-                        Title
-                      </th>
-                      <th
-                        scope="col"
-                        className="relative py-3.5 pl-3 pr-4 sm:pr-0"
-                      >
-                        <span className="sr-only">Edit</span>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="divide-y divide-gray-200">
-                    {contracts.map((person) => (
-                      <tr key={person.name}>
-                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                          {person.name}
-                        </td>
-                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          {person.title}
-                        </td>
-                        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                          <a
-                            href="#"
-                            className="text-blue-600 hover:text-blue-900"
-                          >
-                            Edit<span className="sr-only">, {person.name}</span>
-                          </a>
-                        </td>
+                {contracts.length > 0 ? (
+                  <table className="min-w-full divide-y divide-gray-300">
+                    <thead>
+                      <tr>
+                        <th
+                          scope="col"
+                          className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                        >
+                          Name
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                        >
+                          Title
+                        </th>
+                        <th
+                          scope="col"
+                          className="relative py-3.5 pl-3 pr-4 sm:pr-0"
+                        >
+                          <span className="sr-only">Edit</span>
+                        </th>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
+                    </thead>
+                    <tbody className="divide-y divide-gray-200">
+                      {contracts.map((person) => (
+                        <tr key={person.name}>
+                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                            {person.name}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            {person.title}
+                          </td>
+                          <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                            <a
+                              href="#"
+                              className="text-blue-600 hover:text-blue-900"
+                            >
+                              Edit
+                              <span className="sr-only">, {person.name}</span>
+                            </a>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                ) : (
+                  <>No Documents Yet!</>
+                )}
               </div>
             </div>
           </div>
