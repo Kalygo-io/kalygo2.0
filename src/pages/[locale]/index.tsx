bed8c405cd4422f3ca753c026796bea7085485c1
Taddy <a@a.com>
2023-06-14 17:24:42 -0400

    increase headling on index landing page

--- src/pages/[locale]/index.tsx
+++ src/pages/[locale]/index.tsx
@@ -45,7 +45,7 @@ export default function Home(props: any) {
           />
           <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
             <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
-              <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
+              <h1 className="max-w-2xl text-5xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
                 {t("landing-page:landing-page-headline")}
               </h1>
               <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
