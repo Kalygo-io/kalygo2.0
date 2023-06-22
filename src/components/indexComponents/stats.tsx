331b5cbc2c955c22c2819755bbb5927ef04eb19a
Taddy <a@a.com>
2023-06-21 22:39:13 -0400

    adjusting stats section to be simpler as per feedback suggestions

--- src/components/indexComponents/stats.tsx
+++ src/components/indexComponents/stats.tsx
@@ -3,58 +3,91 @@ import { useTranslation } from "next-i18next";
 export function Stats() {
   const { t } = useTranslation();
 
+  const stats = [
+    {
+      id: 1,
+      headline: t("common:stats.stat1_headline"),
+      subheadline: t("common:stats.stat1_subheadline"),
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+          />
+        </svg>
+      ),
+    },
+    {
+      id: 2,
+      headline: t("common:stats.stat2_headline"),
+      subheadline: t("common:stats.stat2_subheadline"),
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+          />
+        </svg>
+      ),
+    },
+    {
+      id: 3,
+      headline: t("common:stats.stat3_headline"),
+      subheadline: t("common:stats.stat3_subheadline"),
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+          />
+        </svg>
+      ),
+    },
+  ];
+
   return (
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            {t("common:stats.headline")}
-          </h2>
-          <p className="mt-6 text-base leading-7 text-gray-600">
-            {t("common:stats.subheadline")}
-          </p>
-        </div>
-        <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
-          <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-white p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start border shadow">
-            <p className="flex-none text-3xl font-bold tracking-tight text-blue-700">
-              {t("common:stats.stat1_headline")}
-            </p>
-            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
-              {/* <p className="text-lg font-semibold tracking-tight text-black">
-                650+ users
-              </p> */}
-              <p className="mt-2 text-base leading-7 text-black">
-                {t("common:stats.stat1_subheadline")}
-              </p>
+        <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+          {stats.map((stat) => (
+            <div
+              key={stat.id}
+              className="mx-auto flex max-w-xs flex-col items-center gap-y-4"
+            >
+              <div>{stat.icon}</div>
+              <dt className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                {stat.headline}
+              </dt>
+              <dd className="text-base leading-7 text-gray-600">
+                {stat.subheadline}
+              </dd>
             </div>
-          </div>
-          <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-white p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44 border shadow">
-            <p className="flex-none text-3xl font-bold tracking-tight text-blue-700">
-              {t("common:stats.stat2_headline")}
-            </p>
-            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
-              {/* <p className="text-lg font-semibold tracking-tight text-black">
-                100% satisfaction
-              </p> */}
-              <p className="mt-2 text-base leading-7 text-black">
-                {t("common:stats.stat2_subheadline")}
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-white p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28  border shadow">
-            <p className="flex-none text-3xl font-bold tracking-tight text-blue-700">
-              {t("common:stats.stat3_headline")}
-            </p>
-            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
-              {/* <p className="text-lg font-semibold tracking-tight text-black">
-                $1Bn+
-              </p> */}
-              <p className="mt-2 text-base leading-7 text-black">
-                {t("common:stats.stat3_subheadline")}
-              </p>
-            </div>
-          </div>
-        </div>
+          ))}
+        </dl>
       </div>
     </div>
   );
---
+++ src/components/indexComponents/stats_stepped.tsx
@@ -0,0 +1,63 @@
+import { useTranslation } from "next-i18next";
+
+export function Stats() {
+  const { t } = useTranslation();
+
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            {t("common:stats.headline")}
+          </h2>
+          <p className="mt-6 text-base leading-7 text-gray-600">
+            {t("common:stats.subheadline")}
+          </p>
+        </div>
+        <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
+          <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-white p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start border shadow">
+            <p className="flex-none text-3xl font-bold tracking-tight text-blue-700">
+              {t("common:stats.stat1_headline")}
+            </p>
+            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+              {/* <p className="text-lg font-semibold tracking-tight text-black">
+                650+ users
+              </p> */}
+              <p className="mt-2 text-base leading-7 text-black">
+                {t("common:stats.stat1_subheadline")}
+              </p>
+            </div>
+          </div>
+          {/* <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-white p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44 border shadow"> */}
+          <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-white p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start border shadow">
+            <p className="flex-none text-3xl font-bold tracking-tight text-blue-700">
+              {t("common:stats.stat2_headline")}
+            </p>
+            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+              {/* <p className="text-lg font-semibold tracking-tight text-black">
+                100% satisfaction
+              </p> */}
+              <p className="mt-2 text-base leading-7 text-black">
+                {t("common:stats.stat2_subheadline")}
+              </p>
+            </div>
+          </div>
+          {/* <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-white p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28  border shadow"> */}
+          <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-white p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start border shadow">
+            <p className="flex-none text-3xl font-bold tracking-tight text-blue-700">
+              {t("common:stats.stat3_headline")}
+            </p>
+            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+              {/* <p className="text-lg font-semibold tracking-tight text-black">
+                $1Bn+
+              </p> */}
+              <p className="mt-2 text-base leading-7 text-black">
+                {t("common:stats.stat3_subheadline")}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
