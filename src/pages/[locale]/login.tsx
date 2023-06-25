ca889f1acd2f452e0268ee75c75edc59b12a5236
Taddy <a@a.com>
2023-06-24 23:23:15 -0400

    add early stages of social login

--- src/pages/[locale]/login.tsx
+++ src/pages/[locale]/login.tsx
@@ -178,12 +178,67 @@ export default function Signin() {
             </div>
           </form>
 
+          <div>
+            <div className="relative mt-10">
+              <div
+                className="absolute inset-0 flex items-center"
+                aria-hidden="true"
+              >
+                <div className="w-full border-t border-gray-200" />
+              </div>
+              <div className="relative flex justify-center text-sm font-medium leading-6">
+                <span className="bg-white px-6 text-gray-500">
+                  {t("log-in-page:or-continue-with")}{" "}
+                </span>
+              </div>
+            </div>
+
+            <div className="mt-6 grid grid-cols-2 gap-4">
+              <button
+                onClick={() => {}}
+                className="flex w-full items-center justify-center gap-3 rounded-md opacity-20 bg-[grey] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
+              >
+                <svg className="h-5 w-5" aria-hidden="true" viewBox="0 0 48 48">
+                  <path
+                    id="a"
+                    d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"
+                  />
+                </svg>
+                <span className="text-sm font-semibold leading-6">Google</span>
+              </button>
+              <button
+                onClick={() => {}}
+                className="flex w-full items-center justify-center gap-3 rounded-md opacity-20 bg-[#1D9BF0] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#1D9BF0]"
+              >
+                <svg
+                  className="h-5 w-5"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                </svg>
+                <span className="text-sm font-semibold leading-6">Twitter</span>
+              </button>
+            </div>
+          </div>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            {t("log-in-page:not-a-member")}{" "}
+            <Link
+              href="/signup-subscription"
+              className="font-semibold leading-6 text-blue-600 hover:text-blue-500"
+            >
+              {t("log-in-page:start-a-free-trial")}
+            </Link>
+          </p>
+
           <p className="mt-10 text-center text-sm text-gray-500">
             <Link
               href="/"
               className="font-semibold leading-6 text-blue-600 hover:text-blue-500"
             >
-              {t("log-in-page:back-to-home")}
+              {t("log-in-page:back-to-home")} &rarr;
             </Link>
           </p>
         </div>
