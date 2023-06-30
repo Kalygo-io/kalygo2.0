cf92a71f2e92d5dccd3717f55eb064dca0b21e00
Taddy <a@a.com>
2023-06-30 15:32:21 -0400

    save version ie: commit hash

--- src/components/indexComponents/mailing.tsx
+++ src/components/indexComponents/mailing.tsx
@@ -46,7 +46,7 @@ export function Mailing() {
   };
 
   return (
-    <div className="bg-white py-16 sm:py-24">
+    <div className="bg-white pt-16 sm:pt-24">
       <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="relative isolate overflow-hidden bg-blue-600 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
           <div className="flex w-full justify-center items-center">
