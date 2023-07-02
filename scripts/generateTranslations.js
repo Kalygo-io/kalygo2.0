9f8fd372f75ad1eeccc1112324eb02fcdff3bca7
anthony gomez <joseph.12082@gmail.com>
2023-07-02 18:42:06 -0400

    generateTranslation.Js file updated. Now it creates the folder, and translates only the values. It takes about 45 sec to translate though

--- scripts/generateTranslations.js
+++ scripts/generateTranslations.js
@@ -2,33 +2,69 @@ const fs = require("fs");
 const { Translate } = require("@google-cloud/translate").v2;
 const sourceFolderPath = "public/locales/en";
 const outputPath = "public/locales";
-// Anthony's Google Cloud credentials. Change before pushing to production
+
+// personal credentials, update before pushing to production
 const projectId = "kalygotranslation2";
 const keyFilename = "keyfile.json";
+
 const translate = new Translate({ projectId, keyFilename });
-// Function to translate a single file
-async function translateFile(file, targetLanguage) {
-  try {
-    const text = fs.readFileSync(`${sourceFolderPath}/${file}`, "utf8");
-    const [translation] = await translate.translate(text, targetLanguage);
-    const outputFile = `${outputPath}/${targetLanguage}/${file}`;
-    fs.writeFileSync(outputFile, translation, "utf8");
-    console.log(`Translated ${file} to ${targetLanguage}`);
-  } catch (error) {
-    console.error(`Translation error: ${error}`);
+
+// function to translate values in files
+async function translateJSONValues(json, targetLanguage) {
+  if (typeof json === "string") {
+    const [translation] = await translate.translate(json, targetLanguage);
+    return translation;
+  } else if (Array.isArray(json)) {
+    const translatedArray = [];
+    for (const item of json) {
+      const translatedItem = await translateJSONValues(item, targetLanguage);
+      translatedArray.push(translatedItem);
+    }
+    return translatedArray;
+  } else if (typeof json === "object") {
+    const translatedObject = {};
+    for (const key in json) {
+      if (json.hasOwnProperty(key)) {
+        const value = json[key];
+        const translatedValue = await translateJSONValues(
+          value,
+          targetLanguage
+        );
+        translatedObject[key] = translatedValue;
+      }
+    }
+    return translatedObject;
+  } else {
+    return json;
   }
 }
-// translate all files in the source folder
+// function to handle the creation of the folder and files.
 async function translateFiles(targetLanguage) {
   try {
     const files = fs.readdirSync(sourceFolderPath);
     for (const file of files) {
-      await translateFile(file, targetLanguage);
+      const inputFilePath = `${sourceFolderPath}/${file}`;
+      const outputFolderPath = `${outputPath}/${targetLanguage}`;
+      if (!fs.existsSync(outputFolderPath)) {
+        fs.mkdirSync(outputFolderPath, { recursive: true });
+      }
+      const outputFilePath = `${outputFolderPath}/${file}`;
+      const inputJSON = JSON.parse(fs.readFileSync(inputFilePath, "utf8"));
+      const translatedJSON = await translateJSONValues(
+        inputJSON,
+        targetLanguage
+      );
+      fs.writeFileSync(
+        outputFilePath,
+        JSON.stringify(translatedJSON, null, 2),
+        "utf8"
+      );
+      console.log(`Translated ${file} to ${targetLanguage}`);
     }
   } catch (error) {
     console.error(`Error reading source folder: ${error}`);
   }
 }
-const targetLanguage = "fr"; // 1 language at a time, folder for targeted language needs to be created first.
 
+const targetLanguage = "fr"; // only 1 language at a time, folders created automatically now
 translateFiles(targetLanguage);
