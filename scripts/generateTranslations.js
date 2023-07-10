b4f496e809f9fec013a12e8fc452b172822e935c
Taddy <a@a.com>
2023-07-09 22:39:40 -0400

    enhanced translation generation script - now the script only translated strings that have been updated in the base language ie: English

--- scripts/generateTranslations.js
+++ scripts/generateTranslations.js
@@ -7,7 +7,8 @@ const outputPath = "public/locales";
 // personal credentials, update before pushing to production
 const projectId = "kalygotranslation2";
 const keyFilename = "keyfile.json";
-const targetLanguages = ["en", "es", "pt", "fr"];
+// const targetLanguages = ["en", "es", "pt", "fr"];
+const targetLanguages = ["es", "pt", "fr"];
 
 const translate = new Translate({ projectId, keyFilename });
 
@@ -29,6 +30,11 @@ async function translateJSONValues(file, val, targetLanguage, path) {
     const oldValue = get(snapshotJSON, path);
     const newValue = val;
 
+    // TODO
+    // The variables ie `{{ VARIABLE_NAME_HERE }}` in the values of each key
+    // need to be recorded before being sent for translation in case the translation service changes the variable name
+    // if the translation service changes the variable name, you must re-write the variable to the original name
+
     console.log("oldValue", snapshotJSON, oldValue, path);
     console.log("newValue", val, newValue, path);
 
@@ -38,7 +44,15 @@ async function translateJSONValues(file, val, targetLanguage, path) {
       const [translation] = await translate.translate(val, targetLanguage);
       return translation;
     } else {
-      return oldValue;
+      if (fs.existsSync(`${outputPath}/${targetLanguage}/${file}`)) {
+        const oldTranslatedFile = JSON.parse(
+          fs.readFileSync(`${outputPath}/${targetLanguage}/${file}`, "utf8")
+        );
+        return get(oldTranslatedFile, path);
+      } else {
+        const [translation] = await translate.translate(val, targetLanguage);
+        return translation;
+      }
     }
   } else if (Array.isArray(val)) {
     const translatedArray = [];
@@ -78,35 +92,39 @@ async function createFiles(targetLanguage) {
   try {
     const files = fs.readdirSync(sourceFolderPath);
     for (const file of files) {
-      const inputFilePath = `${sourceFolderPath}/${file}`;
-      const outputFolderPath = `${outputPath}/${targetLanguage}`;
-      if (!fs.existsSync(outputFolderPath)) {
-        fs.mkdirSync(outputFolderPath, { recursive: true });
+      if (file.endsWith(".json")) {
+        const inputFilePath = `${sourceFolderPath}/${file}`;
+        const outputFolderPath = `${outputPath}/${targetLanguage}`;
+        if (!fs.existsSync(outputFolderPath)) {
+          fs.mkdirSync(outputFolderPath, { recursive: true });
+        }
+        const outputFilePath = `${outputFolderPath}/${file}`;
+        const inputJSON = JSON.parse(fs.readFileSync(inputFilePath, "utf8"));
+        const translatedJSON = await translateJSONValues(
+          file,
+          inputJSON,
+          targetLanguage,
+          ""
+        );
+
+        console.log("*** translatedJSON ***", translatedJSON);
+
+        // save snapshot
+        fs.writeFileSync(
+          `${sourceFolderPath}/${file}.snapshot`,
+          JSON.stringify(inputJSON, null, 2),
+          "utf8"
+        );
+
+        fs.writeFileSync(
+          outputFilePath,
+          JSON.stringify(translatedJSON, null, 2),
+          "utf8"
+        );
+        console.log(`Translated ${file} to ${targetLanguage}`);
       }
-      const outputFilePath = `${outputFolderPath}/${file}`;
-      const inputJSON = JSON.parse(fs.readFileSync(inputFilePath, "utf8"));
-      const translatedJSON = await translateJSONValues(
-        file,
-        inputJSON,
-        targetLanguage,
-        ""
-      );
 
-      // save snapshot
-      fs.writeFileSync(
-        `${sourceFolderPath}/${file}.snapshot`,
-        JSON.stringify(inputJSON, null, 2),
-        "utf8"
-      );
-
-      fs.writeFileSync(
-        outputFilePath,
-        JSON.stringify(translatedJSON, null, 2),
-        "utf8"
-      );
-      console.log(`Translated ${file} to ${targetLanguage}`);
-
-      break;
+      // break;
     }
   } catch (error) {
     console.error(`Error reading source folder: ${error}`);
@@ -115,6 +133,11 @@ async function createFiles(targetLanguage) {
 
 // new function made to handle languages in parallel
 async function translateFilesParallel(targetLanguages) {
+  // for (let i = 0; i < targetLanguages.length; i++) {
+  //   console.log(targetLanguages[i]);
+  //   createFiles(targetLanguages[i]);
+  // }
+
   const promises = targetLanguages.map((targetLanguage) =>
     createFiles(targetLanguage)
   );
