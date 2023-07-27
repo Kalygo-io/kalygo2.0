c2618aca836c5c48f18f898cc579cf45a8f0a7f0
Taddy <a@a.com>
2023-07-27 04:16:31 -0400

    hide Twitter button

--- src/pages/[locale]/login.tsx
+++ src/pages/[locale]/login.tsx
@@ -235,7 +235,7 @@ export default function Signin() {
               </div>
             </div>
 
-            <div className="mt-6 grid grid-cols-2 gap-4">
+            <div className="mt-6 grid grid-cols-1 gap-4">
               <button
                 onClick={() => googleLogin()}
                 className="flex w-full items-center justify-center gap-3 rounded-md bg-[black] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F] hover:bg-[grey]"
@@ -274,7 +274,7 @@ export default function Signin() {
                 </svg>
                 <span className="text-sm font-semibold leading-6">Google</span>
               </button>
-              <button
+              {/* <button
                 onClick={() => {}}
                 className="flex w-full items-center justify-center gap-3 rounded-md opacity-20 bg-[#1D9BF0] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#1D9BF0]"
               >
@@ -287,7 +287,7 @@ export default function Signin() {
                   <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
                 </svg>
                 <span className="text-sm font-semibold leading-6">Twitter</span>
-              </button>
+              </button> */}
             </div>
           </div>
 
