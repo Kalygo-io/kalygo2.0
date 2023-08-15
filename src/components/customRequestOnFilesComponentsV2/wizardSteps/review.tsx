e3e4444b49f7428d5aabc86fd880cbc38be9150e
Taddy <a@a.com>
2023-08-15 15:03:19 -0400

    add styling to the custom request tool and the Buy Credits Page

--- src/components/customRequestOnFilesComponentsV2/wizardSteps/review.tsx
+++ src/components/customRequestOnFilesComponentsV2/wizardSteps/review.tsx
@@ -83,12 +83,13 @@ export function Review(props: Props) {
                   <div>
                     <div className="mt-2">
                       <textarea
+                        disabled
                         readOnly
                         defaultValue={prompt || "No prompt provided"}
                         rows={4}
                         name="prompt"
                         id="prompt"
-                        className={`block w-full rounded-md border-0 py-1.5 ${
+                        className={`block w-full rounded-md border-0 py-1.5 bg-slate-100 ${
                           prompt ? "text-gray-800" : "text-gray-400"
                         } shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6`}
                       />
--- src/pages/[locale]/dashboard/buy-credits.tsx
+++ src/pages/[locale]/dashboard/buy-credits.tsx
@@ -60,7 +60,7 @@ export default function Document() {
         <title>{t("seo:dashboard-page-seo-meta-title")}</title>
       </Head>
       <LayoutDashboard>
-        <div className="px-4 sm:p-6 lg:p-8">
+        <div className="p-4 sm:p-6 lg:p-8">
           <div className="sm:flex sm:items-center">
             <div className="sm:flex-auto">
               <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
