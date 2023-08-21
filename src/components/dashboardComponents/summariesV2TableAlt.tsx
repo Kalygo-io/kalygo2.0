4aef78ae8e9cfd9ae553ab9213cad71be47ef0a6
Taddy <a@a.com>
2023-08-21 16:05:13 -0400

    Show Prompt Preview Text On Summary Overview Table

--- src/components/dashboardComponents/summariesV2TableAlt.tsx
+++ src/components/dashboardComponents/summariesV2TableAlt.tsx
@@ -1,4 +1,5 @@
 import { useAppContext } from "@/context/AppContext";
+import { ScanningMode } from "@/types/ScanningMode";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 
@@ -62,49 +63,82 @@ export function SummariesV2TableAlt(p: P) {
               >
                 Mode
               </th>
+              <th
+                scope="col"
+                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+              >
+                Preview
+              </th>
               <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
                 <span className="sr-only">View</span>
               </th>
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-200 bg-white">
-            {summaries.map((summary) => (
-              <tr key={summary.id}>
-                {/* <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+            {summaries.map((summary) => {
+              return (
+                <tr key={summary.id}>
+                  {/* <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
                   {summary.id}
                 </td> */}
-                <td className="hidden whitespace-nowrap px-3 py-4 text-sm text-gray-500 sm:table-cell">
-                  <div className="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
-                    <p>
-                      <time
-                        dateTime={new Date(summary.createdAt).toLocaleString()}
-                      >
-                        {new Date(summary.createdAt).toLocaleString()}
-                      </time>
-                    </p>
-                  </div>
-                </td>
-                <td className="hidden whitespace-nowrap px-3 py-4 text-sm text-gray-500 lg:table-cell">
-                  {summary.model}
-                </td>
-                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                  {summary.mode}
-                </td>
-                <td className="whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                  <button
-                    className="text-blue-600 hover:text-blue-900"
-                    onClick={() => {
-                      router.push(
-                        `/dashboard/summary-v2?summary-v2-id=${summary.id}`
-                      );
-                    }}
-                  >
-                    {t("dashboard-page:index.summaries-v2.table.view")}
-                    <span className="sr-only">{summary.id}</span>
-                  </button>
-                </td>
-              </tr>
-            ))}
+                  <td className="hidden whitespace-nowrap px-3 py-4 text-sm text-gray-500 sm:table-cell">
+                    <div className="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
+                      <p>
+                        <time
+                          dateTime={new Date(
+                            summary.createdAt
+                          ).toLocaleString()}
+                        >
+                          {new Date(summary.createdAt).toLocaleString()}
+                        </time>
+                      </p>
+                    </div>
+                  </td>
+                  <td className="hidden whitespace-nowrap px-3 py-4 text-sm text-gray-500 lg:table-cell">
+                    {summary.model}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                    {summary.mode}
+                  </td>
+
+                  {summary.mode === ScanningMode.EACH_FILE_IN_CHUNKS ? (
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {summary?.summary[0]?.summary[0]?.chunkSummary?.slice(
+                        0,
+                        16
+                      )}
+                      ...
+                    </td>
+                  ) : summary.mode === ScanningMode.EACH_FILE_OVERALL ? (
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {summary.summary[0].summary.slice(0, 16)}...
+                    </td>
+                  ) : summary.mode === ScanningMode.OVERALL ? (
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {summary.summary[0].summary.slice(0, 16)}...
+                    </td>
+                  ) : (
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      TODO
+                    </td>
+                  )}
+
+                  <td className="whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                    <button
+                      className="text-blue-600 hover:text-blue-900"
+                      onClick={() => {
+                        router.push(
+                          `/dashboard/summary-v2?summary-v2-id=${summary.id}`
+                        );
+                      }}
+                    >
+                      {t("dashboard-page:index.summaries-v2.table.view")}
+                      <span className="sr-only">{summary.id}</span>
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
