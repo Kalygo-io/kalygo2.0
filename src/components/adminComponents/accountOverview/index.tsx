bea1e83941ea948c0bb01193393ef5e43d4af51f
Taddy <a@a.com>
2023-09-24 23:49:06 -0400

    hide scrollbars for taste

--- src/components/adminComponents/accountOverview/index.tsx
+++ src/components/adminComponents/accountOverview/index.tsx
@@ -29,7 +29,7 @@ export function AccountOverview(p: P) {
   return (
     <main>
       <header className="border-b border-white/5">
-        <nav className="flex overflow-x-auto py-4">
+        <nav className="flex overflow-x-auto py-4 no-scrollbar">
           <ul
             role="list"
             className="flex min-w-full flex-none gap-x-6 px-4 text-sm font-semibold leading-6 text-gray-400 sm:px-6 lg:px-8"
