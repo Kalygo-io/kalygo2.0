8ceca88e3b78b7a3078605963e1a949e1fb7fc72
Taddy <a@a.com>
2023-10-19 19:10:34 -0400

    fix minor bug for summaryV3 entry in jobQueue

--- src/components/queueComponents/jsxForJobType/summaryV3.tsx
+++ src/components/queueComponents/jsxForJobType/summaryV3.tsx
@@ -76,21 +76,17 @@ export const SummaryV3Job = (props: {
             </button>
           </>
         )}
-        {job?.progress === 100 && job?.returnvalue?.summaryV2Id && (
+        {job?.progress === 100 && job?.returnvalue?.summaryV3Id && (
           <button
             type="button"
             className="mx-0.5 inline-flex items-center gap-x-2 rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
             onClick={() => {
               router.push(
-                `/dashboard/summary-v3?summary-v3-id=${job?.returnvalue?.summaryV2Id}`
+                `/dashboard/summary-v3?summary-v3-id=${job?.returnvalue?.summaryV3Id}`
               );
             }}
           >
             {t("dashboard-page:queue.view")}
-            {/* <DocumentMagnifyingGlassIcon
-              className="-mr-0.5 h-5 w-5"
-              aria-hidden="true"
-            /> */}
           </button>
         )}
       </span>
