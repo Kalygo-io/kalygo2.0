cca42d0b07cc1f4ed4984685037a691ec0b0aca1
Taddy <a@a.com>
2023-10-25 19:42:34 -0400

    whitelisting > blacklisting

--- src/components/customRequestOnFilesComponentsV3/wizardSteps/review.tsx
+++ src/components/customRequestOnFilesComponentsV3/wizardSteps/review.tsx
@@ -154,7 +154,6 @@ export function Review(props: Props) {
               );
               const customRequestResponse = await customRequest;
               console.log("customRequestResponse", customRequestResponse);
-
               const detectedLng = navigatorLangDetector();
               router.push(`/${detectedLng}/dashboard/queue`);
               infoToast(t("toast-messages:custom-request-is-processing"));
@@ -162,14 +161,11 @@ export function Review(props: Props) {
               errorToast(e.toString());
             }
           }}
-          disabled={files.length === 0 || customizations?.prompt === ""}
+          disabled={!(files.length > 0 && customizations?.prompt === "")}
           className={`${
-            !files ||
-            files.length === 0 ||
-            !customizations ||
-            customizations?.prompt === ""
-              ? "opacity-50"
-              : "opacity-100"
+            files.length > 0 && customizations?.prompt === ""
+              ? "opacity-100"
+              : "opacity-50"
           } mt-2 flex justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600`}
         >
           {t("dashboard-page:custom-request-v3.send")!}
