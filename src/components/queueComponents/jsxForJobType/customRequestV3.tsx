3ab002c17b077f2c7494469d11dad197eb54aee1
Taddy <a@a.com>
2023-10-26 01:48:15 -0400

    showing scan mode in job queue now

--- src/components/queueComponents/jsxForJobType/customRequestV3.tsx
+++ src/components/queueComponents/jsxForJobType/customRequestV3.tsx
@@ -38,16 +38,16 @@ export const CustomRequestV3 = (props: {
               The Prompt
             </dd>
           </div> */}
-          {/* <div className="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+          <div className="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm font-medium leading-6 text-gray-900">
-              {t("dashboard-page:queue.file-name")}
+              {t("dashboard-page:queue.custom-request-v3.scan-mode")}
             </dt>
             <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
               <ul>
-                <li>{job?.data?.params?.file?.originalname}</li>
+                <li>{job?.data?.params?.customizations?.scanMode}</li>
               </ul>
             </dd>
-          </div> */}
+          </div>
           <div className="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm font-medium leading-6 text-gray-900">
               {job?.data?.params?.files
--- src/components/queueComponents/jsxForJobType/summaryV3.tsx
+++ src/components/queueComponents/jsxForJobType/summaryV3.tsx
@@ -27,7 +27,7 @@ export const SummaryV3Job = (props: {
           </div>
           <div className="px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt className="text-sm font-medium leading-6 text-gray-900">
-              {t("dashboard-page:queue.summary-v3.mode")}
+              {t("dashboard-page:queue.summary-v3.scan-mode")}
             </dt>
             <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
               {job?.data?.params?.customizations?.scanMode}
